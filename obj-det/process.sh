231050f230804a57846bb6d2637aab875220f361
AnsuFrancis <ansu.francis@oneconvergence.com>
2019-02-14 16:32:40 +0530

    script

---
+++ obj-det/process.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+DATA_DIR="${DATUMS_PATH}/${DATASET_NAME}"
+MODEL_DIR="${MODEL_PATH}/${MODEL_NAME}"
+DATA_EXTRACT_PATH="/tmp/object-detection/TFRecords"
+MODEL_EXTRACT_PATH="/tmp/object-detection/Model"
+mkdir -p $DATA_EXTRACT_PATH
+mkdir -p $MODEL_EXTRACT_PATH
+#extract datasets
+for file in $DATA_DIR/*; do
+	if [[ $(file --mime-type -b $file) == application/gzip ]] || [[ $(file --mime-type -b $file) == application/x-tar ]]; then
+		echo "tar file"
+		tar -xvf $file -C $DATA_EXTRACT_PATH
+	elif [[ $(file --mime-type -b $file) == application/zip ]]; then
+		echo "zip file"
+		unzip $file -d $DATA_EXTRACT_PATH
+	else
+		echo "unsupported format"
+	fi
+done
+
+#Extract models
+for file in $MODEL_DIR/*; do
+        if [[ $(file --mime-type -b $file) == application/gzip ]] || [[ $(file --mime-type -b $file) == application/x-tar ]]; then
+                echo "tar file"
+                tar -xvf $file -C $MODEL_EXTRACT_PATH
+        elif [[ $(file --mime-type -b $file) == application/zip ]]; then
+                echo "zip file"
+                unzip $file -d $MODEL_EXTRACT_PATH
+        else
+                echo "unsupported format"
+        fi
+done
+
+cp  $HYPERPARAMS_JSON_FILEPATH $HOME/pipeline.config
+sed -i "s|DATA_PATH|"${DATA_EXTRACT_PATH}"|g" $HOME/pipeline.config
+sed -i "s|MODEL_PATH|"${MODEL_EXTRACT_PATH}"|g" $HOME/pipeline.config
