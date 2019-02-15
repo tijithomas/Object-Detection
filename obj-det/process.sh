e9fc3b3aa7eb1a4ff28204275217b631fc081a43
TIJI K THOMAS <42336597+tijithomas@users.noreply.github.com>
2019-02-15 11:39:47 +0530

    Update process.sh

--- obj-det/process.sh
+++ obj-det/process.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 DATA_DIR="${DATUMS_PATH}/${DATASET_NAME}"
 MODEL_DIR="${MODEL_PATH}/${MODEL_NAME}"
-DATA_EXTRACT_PATH="/tmp/object-detection"
-MODEL_EXTRACT_PATH="/tmp/object-detection"
-mkdir -p $DATA_EXTRACT_PATH
-mkdir -p $MODEL_EXTRACT_PATH
-#extract datasets
+EXTRACT_PATH="/tmp/object-detection"
+mkdir -p $EXTRACT_PATH
+cp  $HYPERPARAMS_JSON_FILEPATH $HOME/pipeline.config
+
+#Extract datasets
 for file in $DATA_DIR/*; do
 	filename=$(basename -- "$file")
 	extension="${filename##*.}" 
 	echo $extension
 	if [[ $extension == "gz" ]]; then
-		echo "tar file"
-		tar -xvf $file -C $DATA_EXTRACT_PATH
+		echo "Data file format: tar"
+		tar -xvf $file -C $EXTRACT_PATH
 	elif [[ $extension == "zip" ]]; then
-		echo "zip file"
-		unzip $file -d $DATA_EXTRACT_PATH
+		echo "Data file format: zip"
+		unzip $file -d $EXTRACT_PATH
 	else
-		echo "unsupported format"
+		echo "Unsupported format"
 	fi
 done
 
@@ -27,15 +27,15 @@ for file in $MODEL_DIR/*; do
         extension="${filename##*.}"
         echo $extension
         if [[ $extension == "gz" ]]; then
-                echo "tar file"
-                tar -xvf $file -C $MODEL_EXTRACT_PATH
+                echo "Model file format: tar"
+                tar -xvf $file -C $EXTRACT_PATH
 	elif [[ $extension == "zip" ]]; then
-                echo "zip file"
-                unzip $file -d $MODEL_EXTRACT_PATH
+                echo "Model file format: zip"
+                unzip $file -d $EXTRACT_PATH
         else
-                echo "unsupported format"
+                echo "Unsupported format"
         fi
 done
-cp  $HYPERPARAMS_JSON_FILEPATH $HOME/pipeline.config
-sed -i "s|DATA_PATH|"${DATA_EXTRACT_PATH}"|g" $HOME/pipeline.config
-sed -i "s|MODEL_PATH|"${MODEL_EXTRACT_PATH}"|g" $HOME/pipeline.config
+
+sed -i "s|DATA_PATH|"${EXTRACT_PATH}"|g" $HOME/pipeline.config
+sed -i "s|MODEL_PATH|"${EXTRACT_PATH}"|g" $HOME/pipeline.config
