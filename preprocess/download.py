ef1e2c83285f9df44ffac90ed3a98aed561fc5ce
AnsuFrancis <ansu.francis@oneconvergence.com>
2019-03-25 14:58:12 +0530

    download images

--- preprocess/download.py
+++ preprocess/download.py
@@ -5,5 +5,7 @@ import tensorflow as tf
 OUTPUT_DIR = os.getenv('OUT_DIR', None)
 opener = urllib.request.URLopener()
 opener.retrieve("http://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz", "./annotations.tar.gz")
+opener.retrieve("http://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz", "./images.tar.gz")
 tf.gfile.Copy("./annotations.tar.gz", OUTPUT_DIR + "/annotations.tar.gz")
+tf.gfile.Copy("./images.tar.gz", OUTPUT_DIR + "/images.tar.gz")
 print("Saved in", OUTPUT_DIR)
