771c59e09b8040cb66bd7d90d2e1eaf9d61b6a3a
AnsuFrancis <ansu.francis@oneconvergence.com>
2019-03-26 12:37:02 +0530

    Preprocess

---
+++ preprocess/download_mount.py
@@ -0,0 +1,8 @@
+import six.moves.urllib as urllib
+import os
+
+OUTPUT_DIR = os.getenv('OUT_DIR', None)
+opener = urllib.request.URLopener()
+opener.retrieve("http://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz", OUTPUT_DIR + "/annotations.tar.gz")
+opener.retrieve("http://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz", OUTPUT_DIR + "/images.tar.gz")
+print("Saved in", OUTPUT_DIR)
