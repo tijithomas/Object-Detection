c0f28b86dc5279f8a944a017d997dfed661d1ccb
AnsuFrancis <ansu.francis@oneconvergence.com>
2019-03-27 12:44:08 +0530

    - Indentation fix

--- preprocess/download_mount.py
+++ preprocess/download_mount.py
@@ -6,17 +6,17 @@ OUTPUT_DIR = os.getenv('OUT_DIR', None)
 target_dir = '/tmp/dataset'
 
 def download():
-	opener = urllib.request.URLopener()
-	opener.retrieve("http://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz", target_dir + "/annotations.tar.gz")
-	opener.retrieve("http://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz", target_dir + "/images.tar.gz")
-	print("Downloaded and saved the dataset: Location: ", target_dir)
+    opener = urllib.request.URLopener()
+    opener.retrieve("http://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz", target_dir + "/annotations.tar.gz")
+    opener.retrieve("http://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz", target_dir + "/images.tar.gz")
+    print("Downloaded and saved the dataset: Location: ", target_dir)
 
 if __name__ == '__main__':
-	if not (os.path.exists(target_dir)):
-		os.mkdir(target_dir)
-	download()
-	for filename in os.listdir(target_dir):
-        	tar = tarfile.open(target_dir + '/' + filename)
-        	tar.extractall(OUTPUT_DIR)
-        	tar.close()
-    	print("Extracted objects and stored. Location: ", OUTPUT_DIR)	
+    if not (os.path.exists(target_dir)):
+        os.mkdir(target_dir)
+    download()
+    for filename in os.listdir(target_dir):
+        tar = tarfile.open(target_dir + '/' + filename)
+        tar.extractall(OUTPUT_DIR)
+        tar.close()
+    print("Extracted objects and stored. Location: ", OUTPUT_DIR)	
