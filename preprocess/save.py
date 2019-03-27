49e38d8fc3e1050ed14bb52fde3f4aecca8e146a
AnsuFrancis <ansu.francis@oneconvergence.com>
2019-03-27 18:47:14 +0530

    typo correction

--- preprocess/save.py
+++ preprocess/save.py
@@ -10,7 +10,7 @@ def compress():
     tf = tarfile.open(TEMP_DIR+"TFRecords.tar.gz", mode="w:gz")
     for filename in os.listdir(DATA_DIR):
         if "pet_faces_train.record" in filename or "pet_faces_val.record" in filename:
-            tf.add(DATA+'/'+filename, arcname='TFRecords/'+filename)
+            tf.add(DATA_DIR+'/'+filename, arcname='TFRecords/'+filename)
     tf.close()
     print("TFRecods compressed and saved")
 
