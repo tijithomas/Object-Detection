29ae94722ce6ad5de71ca45f257d909c34c69b31
Ansu Francis <36500728+AnsuFrancis@users.noreply.github.com>
2019-04-12 01:47:20 +0530

    Update process.sh

--- preprocess_final/process.sh
+++ preprocess_final/process.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+if [ $# -ne 1 ]; then
+    echo "Usage : bash process.sh <c/n>"
+    exit 1
+fi
+
+echo "Confg file path : $1
 
 DATA_DIR="${DATUMS_PATH}/${DATASET_NAME}"
 MODEL_DIR="${MODEL_PATH}/${MODEL_NAME}"
 
 #Set datset path in pipeline.config file
-sed -i "s|DATA_PATH|"${DATA_DIR}"|g" ./pipeline.config
+sed -i "s|DATA_PATH|"${DATA_DIR}"|g" $1
 
 EXTRACT_PATH="/tmp/object-detection"
 mkdir -p $EXTRACT_PATH
@@ -28,4 +34,4 @@ for file in $MODEL_DIR/*; do
 	done
 
 #Set the model path in pipeline.config file to the extracted path
-sed -i "s|MODEL_PATH|"${EXTRACT_PATH}"|g" ./pipeline.config
+sed -i "s|MODEL_PATH|"${EXTRACT_PATH}"|g" $1
